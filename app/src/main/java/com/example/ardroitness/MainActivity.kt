10f07a40ed3bcd4ae12c6b07400eb1f569b41b2e
ryanchutx <ryan.chu@utexas.edu>
2022-02-19 14:32:41 -0600

    Finished Image Buttons and Activity Routing from Home Page
    Image buttons for all artists and linking to corresponding pages

--- app/src/main/java/com/example/ardroitness/MainActivity.kt
+++ app/src/main/java/com/example/ardroitness/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.ardroitness
 
 
+import Nara
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ardroitness.Adapter.ItemAdapter
 
@@ -18,7 +20,35 @@ class MainActivity : AppCompatActivity() {
         val button = findViewById<Button>(R.id.button_random)
         button.setOnClickListener(){
             //intent = Intent(this, RandomPage::class.java)
+            intent = Intent(this, RandomPage::class.java)
+            startActivity(intent)
+        }
+
+        val goghButton = findViewById<ImageButton>(R.id.gogh_button)
+        button.setOnClickListener(){
+            //intent = Intent(this, RandomPage::class.java)
+            intent = Intent(this, Gogh::class.java)
+            startActivity(intent)
+        }
+
+        val daliButton = findViewById<ImageButton>(R.id.dali_button)
+        button.setOnClickListener(){
+            //intent = Intent(this, RandomPage::class.java)
+            intent = Intent(this, Dali::class.java)
+            startActivity(intent)
+        }
+
+        val picassoButton = findViewById<ImageButton>(R.id.picasso_button)
+        button.setOnClickListener(){
+            //intent = Intent(this, RandomPage::class.java)
             intent = Intent(this, Picasso::class.java)
+            startActivity(intent)
+        }
+
+        val naraButton = findViewById<ImageButton>(R.id.nara_button)
+        button.setOnClickListener(){
+            //intent = Intent(this, RandomPage::class.java)
+            intent = Intent(this, Nara::class.java)
             startActivity(intent)
         }
 
