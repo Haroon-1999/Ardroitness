024b9fbf7355a3665398c2e7f9715dee89851c4b
Hardy <hardyxu@utexas.edu>
2022-02-19 15:45:57 -0600

    Fix the ramdom button

--- app/src/main/java/com/example/ardroitness/RandomPage.kt
+++ app/src/main/java/com/example/ardroitness/RandomPage.kt
@@ -2,46 +2,19 @@ package com.example.ardroitness
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ardroitness.Adapter.ItemAdapter
 import com.example.ardroitness.Art.Art
 import com.example.ardroitness.dataSource.DataSource
-import java.security.AccessController.getContext
+import kotlin.random.Random
 
 class RandomPage : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_gallery)
 
-        var artist:Int = (1..4).random()
-        var artwork:Int = (0..3).random()
-        var data = DataSource().loadDali()
-
-        if (artist == 1) {
-            var dataset = DataSource().loadPicasso()
-            var data = listOf(dataset[artwork])
-        }
-        if (artist == 2) {
-            var dataset = DataSource().loadDali()
-            var data = listOf(dataset[artwork])
-        }
-        if (artist == 3) {
-            var dataset = DataSource().loadGogh()
-            var data = listOf(dataset[artwork])
-        }
-        if (artist == 4) {
-            var dataset = DataSource().loadNara()
-            var data = listOf(dataset[artwork])
-        }
-
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.adapter = ItemAdapter(this, data)
-
-        recyclerView.setHasFixedSize(true)
-
+        val numberOfMethods = 3
         /*
         val button2 = findViewById<Button>(R.id.button_home)
         val context = button2.context
@@ -49,6 +22,13 @@ class RandomPage : AppCompatActivity(){
             intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }*/
+        when (Random.nextInt(numberOfMethods)) {
+            0 -> intent = Intent(this, Gogh::class.java)
+            1 -> intent = Intent(this, Nara::class.java)
+            2 -> intent = Intent(this, Picasso::class.java)
+            else -> intent = Intent(this, Dali::class.java)
+        }
+        startActivity(intent)
 
 
     }
